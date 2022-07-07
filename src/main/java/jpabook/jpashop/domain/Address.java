a4bad5e51ccc842d0085a8dee57ad2652c99daca
eyi-jin <jmj2588547@naver.com>
2022-07-08 01:19:14 +0900

    [Add] add Entity class done

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -5,9 +5,19 @@ import lombok.Getter;
 import javax.persistence.Embeddable;
 
 @Embeddable //jpa의 내장 타입을 나타냄
-@Getter
+@Getter //값 타입은 getter만 만든다
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address(){
+
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -22,5 +22,6 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
+    @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 }
