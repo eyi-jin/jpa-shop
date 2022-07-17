b65ef4568945df6868835d2995d731580bef5c88
eyi-jin <jmj2588547@naver.com>
2022-07-17 19:08:24 +0900

    [Add] Add API order find version1 - using Entity

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,23 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.hibernate.Hibernate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+//		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true); 이걸 하면 레이지 로딩에 있는 것 다 들고옴, 당장에 필요없을 때는 이거 안해도 됨
+		return hibernate5Module;
 	}
 
 }
